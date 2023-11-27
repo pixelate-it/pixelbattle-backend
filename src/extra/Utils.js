8034e835c9ca84ffc0508b2de592a5c7aed8727a
mirdukkkkk <karmulav@gmail.com>
2023-11-27 23:30:40 +0300

    token gen change

--- src/extra/Utils.js
+++ src/extra/Utils.js
@@ -2,8 +2,8 @@ const { v4: uuidv4 } = require('uuid');
 
 module.exports = {
     generateToken(date = null) {
-        const token = `${uuidv4()}.${(date ?? Date.now()).toString(36)}.${uuidv4()}`
-        return token; // на данный момент у токена фиксированная длинна 82 символа!
+        const token = `${(uuidv4() + '.' + uuidv4()).replace(/-/g, '')}.${(date ?? Date.now()).toString(36)}`
+        return token; // на данный момент у токена фиксированная длинна 72 символа!
     },
     translateHex(hex) {
         const r = parseInt(hex.slice(1, 3), 16);
