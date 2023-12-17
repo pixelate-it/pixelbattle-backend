d8a7813fcaf59553b04a9f55c9b893bee893b671
mirdukkkkk <karmulav@gmail.com>
2023-12-18 06:22:40 +1100

    Create UserDataCache.js

---
+++ src/helpers/UserDataCache.js
@@ -0,0 +1,26 @@
+class UserDataCache {
+    #expiresOn;
+    constructor(data) {
+        this.token = data.token;
+        this.userID = data.userID;
+        this.username = data.username;
+        this.cooldown = data.cooldown;
+        this.tag = data.tag;
+
+        this.breath();
+    }
+
+    get expiresOn() {
+        return this.#expiresOn;
+    }
+
+    set(prop, val) {
+        return this[prop] = val;
+    }
+
+    breath() {
+        this.#expiresOn = Date.now() + parseInt(process.env.expiresIn || 360) * 1000;
+    }
+}
+
+module.exports = UserDataCache;
