5ddb9d5ee3f10d165e7e6b03cc34ea7fcd292ed7
mirdukkkkk <karmulav@gmail.com>
2024-01-21 23:38:31 +0700

    perf: now png is used instead of bmp
    why better?
    - unlike bmp, png weighs 12 times less

--- src/routes/requestPixels.js
+++ src/routes/requestPixels.js
@@ -1,9 +1,10 @@
-const bmp = require('@wokwi/bmp-ts').default;
+//const bmp = require('@wokwi/bmp-ts').default;
+const png = require('fast-png');
 const { translateHex } = require('../extra/Utils');
 
 module.exports = ({ canvas, game }) => ({
     method: 'GET',
-    path: '/pixels.bmp',
+    path: '/pixels.png',
     schema: {},
     config: {
         rateLimit: {
@@ -13,15 +14,21 @@ module.exports = ({ canvas, game }) => ({
     },
     async handler(_, response) {
         return response
-            .header('Content-Type', 'image/bmp')
+            .header('Content-Type', 'image/png')
             .code(200)
             .send(
-                bmp.encode({
+                png.encode({
                     width: game.width,
                     height: game.height,
-                    data: new Uint8Array(canvas.pixels.map(pix => translateHex(pix.color)).flat()),
-                    bitPP: 24,
-                }).data
+                    channels: 3,
+                    data: new Uint8Array(canvas.pixels.map(pix => translateHex(pix.color)).flat())
+                })
+                //bmp.encode({
+                //    width: game.width,
+                //    height: game.height,
+                //    data: new Uint8Array(canvas.pixels.map(pix => translateHex(pix.color)).flat()), // change in utils to [255, b, g, r];
+                //    bitPP: 24,
+                //}).data
             );
     }
 });
